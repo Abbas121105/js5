038b0d0e9297823d70574481ab1ce009ca174ddd
abbas121105 <abbaskerimovv@gmail.com>
2025-09-16 17:07:57 +0400

    js5

---
+++ app.js
@@ -0,0 +1,7 @@
+let job = prompt ("add a post you want");
+
+console.log(job.length);
+console.log(job.includes("e"));
+console.log(job.startsWith("Code"));
+console.log(job.endsWith("Code"));
+console.log(job.replaceAll("t", "####"));
